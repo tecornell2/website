b6c79bcb862b3073b223b29538127da06696c1bd
Taryn Cornell <77257369+tecornell2@users.noreply.github.com>
2025-03-20 23:20:55 -0700

    Update collapse-section.js

--- collapse-section.js
+++ collapse-section.js
@@ -4,11 +4,11 @@ var i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+    var dropdown = this.nextElementSibling;
+    if (dropdown.style.display === "block") {
+      dropdown.style.display = "none";
     } else {
-      content.style.display = "block";
+      dropdown.style.display = "block";
     }
   });
 }
