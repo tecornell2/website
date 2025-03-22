20fc1a118a6ed29e95dccf93e0b673c738967c7f
Taryn Cornell <77257369+tecornell2@users.noreply.github.com>
2025-03-22 10:49:34 -0700

    Update collapse-section.js

--- collapse-section.js
+++ collapse-section.js
@@ -1,3 +1,4 @@
+////////// COLLAPSIBLE DROPDOWN
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
@@ -11,4 +12,25 @@ for (i = 0; i < coll.length; i++) {
       dropdown.style.display = "block";
     }
   });
+}
+
+////////// IMAGE MODAL
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
 }
